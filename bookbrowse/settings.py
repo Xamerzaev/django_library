9d7a78e2ad26d212045afcbae2809323579c32c6
Mansur <mansur.ham@mail.ru>
2022-05-17 00:29:21 +0300

    fix

--- bookbrowse/settings.py
+++ bookbrowse/settings.py
@@ -26,8 +26,7 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 STATIC_URL = STATIC_HOST + "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = look_folder_tree(STATIC_ROOT)
 # Quick-start development settings - unsuitable for production
