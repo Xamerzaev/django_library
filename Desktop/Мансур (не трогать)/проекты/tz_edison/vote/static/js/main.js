55f0567c30c208c032f65a1d9acb42b916eb7a01
Xammirzaev <ramzanxam95reg@gmail.com>
2022-08-19 22:17:50 +0300

    save arezzo files

--- Desktop/Мансур (не трогать)/проекты/tz_edison/vote/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    var socketStart = new WebSocket("ws://" + window.location.host + "/start/");
-    socketStart.onmessage = function (e) {
-        window.location.replace(location.href.replace('/polls/', '/polls/first/'));
-
-    };
-
-});
